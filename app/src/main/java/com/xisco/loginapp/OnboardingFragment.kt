ce6d9772555fa292ac4628b1246b0f4dc852bf79
Unekwe Francis <unekwenchekwube@gmail.com>
2022-05-25 20:16:24 +0100

    data binding for onboarding fragment done

--- app/src/main/java/com/xisco/loginapp/OnboardingFragment.kt
+++ app/src/main/java/com/xisco/loginapp/OnboardingFragment.kt
@@ -5,14 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.xisco.loginapp.databinding.FragmentOnboardingBinding
 
 class OnboardingFragment : Fragment() {
+            private  lateinit var  binding: FragmentOnboardingBinding
 
             override fun onCreateView(
                         inflater: LayoutInflater, container: ViewGroup?,
                         savedInstanceState: Bundle?
             ): View? {
                         // Inflate the layout for this fragment
-                        return inflater.inflate(R.layout.fragment_onboarding, container, false)
+                        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_onboarding,container,false)
+
+                        binding.btnRegister.setOnClickListener{
+                                    Toast.makeText(activity, "Register clicked", Toast.LENGTH_SHORT).show()
+                        }
+                        return  binding.root
+//                        return inflater.inflate(R.layout.fragment_onboarding, container, false)
             }
 }
