a054e42513439b141a77a72b3f2a1852b6bca9b5
Unekwe Francis <unekwenchekwube@gmail.com>
2022-05-26 07:44:52 +0100

    added nav controller and successfully navigated from onboarding fragment

--- app/src/main/java/com/xisco/loginapp/OnboardingFragment.kt
+++ app/src/main/java/com/xisco/loginapp/OnboardingFragment.kt
@@ -7,22 +7,34 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 import com.xisco.loginapp.databinding.FragmentOnboardingBinding
 
 class OnboardingFragment : Fragment() {
-            private  lateinit var  binding: FragmentOnboardingBinding
+            private lateinit var binding: FragmentOnboardingBinding
 
             override fun onCreateView(
                         inflater: LayoutInflater, container: ViewGroup?,
                         savedInstanceState: Bundle?
             ): View? {
                         // Inflate the layout for this fragment
-                        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_onboarding,container,false)
+                        binding = DataBindingUtil.inflate(
+                                    inflater,
+                                    R.layout.fragment_onboarding,
+                                    container,
+                                    false
+                        )
 
-                        binding.btnRegister.setOnClickListener{
-                                    Toast.makeText(activity, "Register clicked", Toast.LENGTH_SHORT).show()
+                        binding.apply {
+                                    btnRegister.setOnClickListener {
+                                                findNavController().navigate(R.id.action_onboardingFragment_to_registerFragment)
+                                    }
+
+                                    btnLogin.setOnClickListener {
+                                                findNavController().navigate(R.id.action_onboardingFragment_to_loginFragment)
+                                    }
                         }
-                        return  binding.root
+                        return binding.root
 //                        return inflater.inflate(R.layout.fragment_onboarding, container, false)
             }
 }
